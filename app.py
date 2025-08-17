0560268657715082842d30d466a0fe430fae0eb3
Manav Kashyap <manavkashyap6708@gmail.com>
2025-08-17 21:30:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from recm import recommend
 df = pd.read_csv("cars_cleaned.csv")
 
 # Convert units
-df["Price (₹ Lakh)"] = (df["Price_USD"] * 83) / 100000
+df["Price (₹ Lakh)"] = (df["Price_USD"] * 87) / 100000
 df["Engine (cc)"] = df["Engine_L"] * 1000
 df["Torque (Nm)"] = df["Torque_lbft"] * 1.35582
 df["0-100 km/h (s)"] = df["ZeroTo60"] * 1.60934 / 0.44704
@@ -27,17 +27,17 @@ if selected_car:
     st.subheader(f"📌 Details of {selected_car}")
     st.write(
         f"""
-        • Year: {car_data['Year']}  
+        • Price: ₹ {car_data['Price (₹ Lakh)']:.2f} Lakh
         • Engine: {car_data['Engine (cc)']:.0f} cc  
         • Power: {car_data['Horsepower']} HP  
         • Torque: {car_data['Torque (Nm)']:.0f} Nm  
         • 0–100 km/h: {car_data['0-100 km/h (s)']:.1f} sec  
-        • Price: ₹ {car_data['Price (₹ Lakh)']:.2f} Lakh
+       
         """
     )
 
     st.subheader("🤝 Recommended Cars for You")
-    recs = recommend(selected_car, top_n=5)
+    recs = recommend(selected_car, top_n=4)
 
     if not recs.empty:
         cols = st.columns(len(recs))
@@ -46,12 +46,13 @@ if selected_car:
                 st.markdown(f"### {row['Make']} {row['Model']}")
                 st.write(
                     f"""
-                    • Year: {row['Year']}  
+                    • Price: ₹ {row['Price_USD']*83/100000:.2f} Lakh 
                     • Engine: {row['Engine_L']*1000:.0f} cc  
                     • Power: {row['Horsepower']} HP  
                     • Torque: {row['Torque_lbft']*1.35582:.0f} Nm  
-                    • Price: ₹ {row['Price_USD']*83/100000:.2f} Lakh
+                    
                     """
                 )
     else:
         st.info("No similar cars found.")
+
